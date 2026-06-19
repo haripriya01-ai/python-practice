1e95a117be3aef6ed3ae2272b8ca0b6af3cf187b
haripriya-csm <haripriyakudikala01@gmail.com>
2026-06-19 14:04:14 +0530

    Add files via upload
    variables

---
+++ main.py
@@ -0,0 +1,17 @@
+age=23
+print("age")
+#to know the type of variable we use type while printing
+print(type(age))
+
+cgpa=8.9
+print(cgpa)
+print(type(cgpa))
+
+name="haripriya"
+print(name)
+print(type(name))
+
+#lots of task have and we want to mark one task as completed we use boolen
+is_completed=True #here the value may be True or False and the first letter must be captial.
+print(is_completed)
+print(type(is_completed))
