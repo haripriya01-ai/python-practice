d8fa3c5195d48b75ccc21155b7f891e8c39835af
haripriya-csm <haripriyakudikala01@gmail.com>
2026-06-19 14:41:12 +0530

    Add files via upload

---
+++ if_elif_else.py
@@ -0,0 +1,11 @@
+age= int(input("Enter your age :"))
+
+if(age>18):
+    print("You can drive")
+elif(age==18):
+    print("Let's check weather you are fit or not") 
+elif(age==0):
+    print("hey you are just born")
+else:
+    print("Sorry you are not allowed to drive")     
+    #this is a simple if_elif_else ladder and we can add number of elif statements in the middle  
--- main.py
+++ main.py
@@ -1,55 +1,6 @@
-'''1.arithmatic operators
-      addition(+),
-      subtraction(-)
-      multiplication(*),
-      division(/)
-      modulus(%)
-      exponentiation(**)
-      floor division(//)'''
-a= 34
-b= 2
-#arithmatic operators
-print("....arthimetic...")
-print("a + b = ", a + b)
-print("a - b = ", a - b)
-print("a * b = ", a * b)
-print("a / b = ", a / b)
-print("a % b = ", a % b)
-print("a ** b = ", a ** b)
-print("a // b = ", a // b)
+#conditionals are the basic logic builders in the code
+age=32
 
-#conditional operators
-print("....conditional....")
-print(a>4)
-print(a<4)
-print(a==4)#is a equals to 4?
-print(a>=4)
-print(a==34)#is a equals to 34?
-print(a!=34)#it is false because a is equal to 34
-
-#logical operators
-#logical operators is to perform operation on two boolens
-print("....and....")
-c=True
-d=False
-print(c and c)#even if their is one false the o/p will be false
-print(c and d)
-print(d and c)
-print(d and d)
-#or
-print("....or....")
-print(c or c)#atleast one true unte true ee
-print(d or d)
-print(c or d)
-print(d or c)
-
-print("....not operator.....")
-print(not(True))
-print(not(False))
-
-print("...assingment...")
-a=32
-print(a)
-a+=3
-print(a)
-
+if(age>18):
+    print("yoou can drive")#if statement is used in python based on the condition
+print("end of programme")    
