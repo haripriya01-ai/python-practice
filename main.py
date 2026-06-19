3a561423f0972120dff4821a483650ad74de6be1
haripriya-csm <haripriyakudikala01@gmail.com>
2026-06-19 14:42:02 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-sentence="coding in python is fun"
-vowels=['a','e','i','o','u']
-sum=0
-for char in sentence.lower():
-    if(char in vowels):
-        sum+=1
-print(f"there are {sum} vowels in sentence")
+# #repeating to print like printing 1 to 100
+# print(1)
+# print(2)
+# print(3)
+# print(5)
+# print(6)
+
+# for i in range(1,6): #the counting is done from 1 to n-1 soo 5
+#     print(i)
+
+for i in range(1,11):
+    print("5 X",i,"=",5*i)
+
