12425961d2bb35dda1c9fe4dc946dc50a0045960
haripriya-csm <haripriyakudikala01@gmail.com>
2026-06-19 14:09:36 +0530

    Add files via upload
    user type

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-age=23
-print("age")
-#to know the type of variable we use type while printing
-print(type(age))
-
-cgpa=8.9
-print(cgpa)
-print(type(cgpa))
-
-name="haripriya"
-print(name)
-print(type(name))
-
-#lots of task have and we want to mark one task as completed we use boolen
-is_completed=True #here the value may be True or False and the first letter must be captial.
-print(is_completed)
-print(type(is_completed))
+a =input("Enter a number")#input word is used to interact with user to give any input he/she wanted to 
+print(a)#it prints the number what the user provided
+b= input("Enter your name:")
+print(b)
