8716a7c7d2e31f19536326c520eb564da90922bd
haripriya-csm <haripriyakudikala01@gmail.com>
2026-06-19 14:40:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,55 @@
-a =input("Enter a number")#input word is used to interact with user to give any input he/she wanted to 
-print(a)#it prints the number what the user provided
-b= input("Enter your name:")
-print(b)
+'''1.arithmatic operators
+      addition(+),
+      subtraction(-)
+      multiplication(*),
+      division(/)
+      modulus(%)
+      exponentiation(**)
+      floor division(//)'''
+a= 34
+b= 2
+#arithmatic operators
+print("....arthimetic...")
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
+print("a ** b = ", a ** b)
+print("a // b = ", a // b)
+
+#conditional operators
+print("....conditional....")
+print(a>4)
+print(a<4)
+print(a==4)#is a equals to 4?
+print(a>=4)
+print(a==34)#is a equals to 34?
+print(a!=34)#it is false because a is equal to 34
+
+#logical operators
+#logical operators is to perform operation on two boolens
+print("....and....")
+c=True
+d=False
+print(c and c)#even if their is one false the o/p will be false
+print(c and d)
+print(d and c)
+print(d and d)
+#or
+print("....or....")
+print(c or c)#atleast one true unte true ee
+print(d or d)
+print(c or d)
+print(d or c)
+
+print("....not operator.....")
+print(not(True))
+print(not(False))
+
+print("...assingment...")
+a=32
+print(a)
+a+=3
+print(a)
+
